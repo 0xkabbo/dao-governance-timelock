10ca8e7fdc80141d89f9604400cca4057a3eff76
Kabbo <161012310+0xkabbo@users.noreply.github.com>
2026-03-06 22:31:56 +0600

    Create GovernanceToken.sol

---
+++ GovernanceToken.sol
@@ -0,0 +1,20 @@
+// SPDX-License-Identifier: MIT
+pragma solidity ^0.8.20;
+
+import "@openzeppelin/contracts/token/ERC20/extensions/ERC20Votes.sol";
+import "@openzeppelin/contracts/access/Ownable.sol";
+
+contract GovernanceToken is ERC20Votes, Ownable {
+    constructor() ERC20("Governance Token", "GTK") EIP712("Governance Token", "1") Ownable(msg.sender) {
+        _mint(msg.sender, 1000000 * 10 ** decimals());
+    }
+
+    // Standard overrides for ERC20Votes
+    function _update(address from, address to, uint256 value) internal override(ERC20, ERC20Votes) {
+        super._update(from, to, value);
+    }
+
+    function nonces(address owner) public view override(ERC20Permit, Nonces) returns (uint256) {
+        return super.nonces(owner);
+    }
+}
